c4e90a180e0adcd67e991d0189190c07192d58f6
bleazorn <only_forgames@hotmail.com>
2021-12-08 20:12:55 +0100

    frame super background check

--- Bank/GUI/frameSuper.py
+++ Bank/GUI/frameSuper.py
@@ -1,5 +1,12 @@
+from background.background import Background
+
+
 class FrameSuper(object):
     def __init__(self, parent, background, loc):
+        if not isinstance(background, Background):
+            print("fault in getting background")
+            # TODO: create own exceptions
+            return
         self.parent = parent
         self.background = background
         self.row, self.col = self.getRowCol(loc)
